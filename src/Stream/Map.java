1119513bbbdabe36674de2bac8a4b490acb9722e
Lucasssi <lucasssi89@gmail.com>
2022-12-13 23:56:33 -0300

    Stream Map

---
+++ src/Stream/Map.java
@@ -0,0 +1,33 @@
+package Stream;
+
+import java.util.function.Consumer;
+import java.util.List;
+import java.util.Arrays;
+import java.util.stream.Stream;
+import java.util.function.UnaryOperator;
+
+public class Map {
+
+    public static void main(String[] args) {
+
+        Consumer<String> print = System.out::print;
+
+        List<String> marcas = Arrays.asList("BMW ", "Audi ", "Honda ");
+        marcas.stream().map(m -> m.toUpperCase()).forEach(print);
+
+        UnaryOperator<String> maiuscula = n -> n.toUpperCase();
+        UnaryOperator<String> primeiraLetra = n -> n.charAt(0) + "";
+        UnaryOperator<String> grito = n -> n + "!!!";
+
+        System.out.println("\n Usando composição...");
+        marcas.stream().map(maiuscula).forEach(print);
+
+        System.out.println("\n Usando composição...");
+        marcas.stream().map(maiuscula).map(primeiraLetra).forEach(print);
+
+        System.out.println("\n Usando composição...");
+        marcas.stream().map(maiuscula).map(primeiraLetra).map(grito).forEach(print);
+
+    }
+
+}
